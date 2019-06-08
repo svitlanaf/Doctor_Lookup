c2df37c4e0bb246304de98b4226c6b69c139173e
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-08 12:39:40 -0700

    Fixed doctor search to return doctors by any query

--- src/DoctorSearch.js
+++ src/DoctorSearch.js
@@ -2,7 +2,7 @@ export class DoctorSearch {
     getDoctor(query) {
         return new Promise(function(resolve, reject) {
             let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=47.609722,-122.333056,100&skip=2&limit=10&user_key=${process.env.exports.apiKey}`
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=47.620422,-122.349358,20&&limit=100&user_key=${process.env.exports.apiKey}`
             request.onload = function() {
                 if (this.status == 200) {
                     resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,23 +5,24 @@ import './styles.css';
 import { DoctorSearch } from './DoctorSearch.js';
 
 $(document).ready(function() {
-    $('#doctorSearch').submit(function() {
-      let query = $('#query').val();
-      $('#query').val("");
-  
-  
-      let DoctorSearch = new DoctorSearch();
-      let promise = DoctorSearch.getDoctor(query);
-  
-      promise.then(function(response) {
-        let body = JSON.parse(response);
-        for(let i= 0; i<body.data.length; i++) {
-        $('.searchResults').append("<li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + " "+ body.data[i].practices[0].accepts_new_patients + " "+ body.data[i].practices[0].phones[0].number + body.data[i].practices[0].website + "</li>");
-    }
-        
-      }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-      });
+    $('#userInput').submit(function(e) {
+       e.preventDefault();
+       let query = $('#query').val();
+       $('#query').val("");
+       let newDoctorSearch = new DoctorSearch();
+       
+       let promise = newDoctorSearch.getDoctor(query);
+       promise.then(function(response) {
+           let body = JSON.parse(response);
+           for(let i= 0; i<body.data.length; i++) {
+               if (body.data[i].practices[0].accepts_new_patients == true) {
+                   $('.searchResults').append("<li>"  + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + " " + body.data[i].specialties[0].actor + " " + body.data[i].practices[0].phones[0].number + body.data[i].practices[0].website + "This doctor is accepting new patients." + "</li>");
+                } else {
+                    $('.searchResults').append("<li>"  + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + " " + body.data[i].specialties[0].actor + " " + body.data[i].practices[0].phones[0].number + body.data[i].practices[0].website + "This doctor is not accepting new patients." + "</li>");
+                }
+            }
+        }, function(error) {
+            $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+        });
     });
-  
-  });
+})
